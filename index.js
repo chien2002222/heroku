272f8ee024fba1a6e0d12d36ec642345acf20f56
chien2002222 <chiendvgch200793@fpt.edu.vn>
2022-09-22 17:08:03 +0700

    ok

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require("http")
+const port = 3000
+
+const app = http.createServer((req, res)=>{
+    res.write("<h1> Hello world</h1>")
+    res.write(
+        "<h2>My nodejs web page has been deployed to Heroku</h2>"
+    )
+    res.end()
+    app.listen(port)
+})
